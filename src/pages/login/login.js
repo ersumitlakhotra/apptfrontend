703ae557b7f59b4c5a33352c16583cd662d9f917
ersumitlakhotra <ersumitlakhotra@gmail.com>
2025-04-17 12:33:52 -0400

    Add Files
    Login page and login css

---
+++ src/pages/login/login.js
@@ -0,0 +1,102 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import {
+  Row,
+  Col,
+  Layout as AntdLayout,
+  Card,
+  Typography,
+  Form,
+  Input,
+  Button,
+  Checkbox,
+} from "antd";
+import "./styles.css";
+
+const { Text, Title } = Typography;
+
+export const Login = () => {
+
+  const CardTitle = (
+    <Title level={3} className="title">
+      Sign in your account
+    </Title>
+  );
+
+  return (
+    <AntdLayout className="layout">
+      <Row
+        justify="center"
+        align="middle"
+        style={{
+          height: "100vh",
+        }}
+      >
+        <Col xs={22}>
+          <div className="container">
+            <Card title={CardTitle} headStyle={{ borderBottom: 0 }}>
+              <Form
+                layout="vertical"
+                requiredMark={false}
+                initialValues={{
+                  remember: false,
+                }}
+              >
+                <Form.Item
+                  name="username"
+                  label="Username"
+                  rules={[{ required: true }]}
+                >
+                  <Input size="large" placeholder="Username" />
+                </Form.Item>
+                <Form.Item
+                  name="password"
+                  label="Password"
+                  rules={[{ required: true }]}
+                  style={{ marginBottom: "12px" }}
+                >
+                  <Input type="password" placeholder="●●●●●●●●" size="large" />
+                </Form.Item>
+                <div style={{ marginBottom: "12px" }}>
+                  <Form.Item name="remember" valuePropName="checked" noStyle>
+                    <Checkbox
+                      style={{
+                        fontSize: "12px",
+                      }}
+                    >
+                      Remember me
+                    </Checkbox>
+                  </Form.Item>
+
+                  <a
+                    style={{
+                      float: "right",
+                      fontSize: "12px",
+                    }}
+                    href="#"
+                  >
+                    Forgot password?
+                  </a>
+                </div>
+                <Button type="primary" size="large" htmlType="submit" block>
+                  Sign in
+                </Button>
+              </Form>
+              <div style={{ marginTop: 8 }}>
+                <Text style={{ fontSize: 12 }}>
+                  Don’t have an account?{" "}
+                  <a href="#" style={{ fontWeight: "bold" }}>
+                    Sign up
+                  </a>
+                </Text>
+              </div>
+            </Card>
+          </div>
+        </Col>
+      </Row>
+    </AntdLayout>
+  );
+};
+
+
+export default Login;
