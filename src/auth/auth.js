01d47b8184662b224d41fb206c4805da8caaa17a
Sumit kumar <ersumitlakhotra@gmail.com>
2026-01-17 21:46:28 -0500

    updating

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -29,17 +29,19 @@ export const isAuthenticated = async() => {
 export const handleAuth = async (email,password) => {
     try {
       const res = await loginAuth(email, password);
-      if (res.status === 200 && res.data.data !== null) {
-        if (Boolean(res.data.data.active)) {
-          setLocalStorageWithExpiry('cid', res.data.data.cid);
-          setLocalStorageWithExpiry('uid', res.data.data.id);
+      if (res.status === 200 && res.data.data.length === 1) {
+        if (Boolean(res.data.data[0].active)) {
+          setLocalStorageWithExpiry('cid', res.data.data[0].cid);
+          setLocalStorageWithExpiry('uid', res.data.data[0].id);
           setLocalStorageWithExpiry('email', email, 60);
           setLocalStorageWithExpiry('password', password, 60); 
-          return { status: res.status, data: res.data.data, message: 'Login Successful'};       
+          return { status: res.status, data: res.data.data[0], message: 'Login Successful'};       
         }
         else
           return { status: 404, data: null, message: 'Your account is deactivated and cannot access the application. Please contact our help desk!' };
       }
+      else if(res.data.data.length > 1)
+        return { status: 404, data: null, message: 'The username is allocated to a separate potal. Ensure that each account has a unique password.' };
       else
         return { status: 404, data: null, message: 'Login Failed. Invalid username or password.' };
     }
